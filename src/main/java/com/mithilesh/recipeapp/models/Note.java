0261d4c0c2c495f926cc46ac46dbd95b0614fd57
Mithilesh Kumar <mithilesh.kumar@phonepe.com>
2018-06-18 20:50:11 +0530

    Added models

---
+++ src/main/java/com/mithilesh/recipeapp/models/Note.java
@@ -0,0 +1,39 @@
+package com.mithilesh.recipeapp.models;
+
+import javax.persistence.*;
+
+@Entity
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Lob
+    private String recipeNotes;
+
+    @OneToOne
+    private Recipe recipe;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
